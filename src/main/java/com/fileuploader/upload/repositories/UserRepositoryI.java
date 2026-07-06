e51b528e50350ce0e104773b7643e7b78796995c
Azu Chukwuemeka <65517040+AzuChukwuemeka@users.noreply.github.com>
2026-07-06 14:23:48 +0100

    refactor: pushing new files built on local development

--- src/main/java/com/fileuploader/upload/repositories/UserRepositoryI.java
+++
@@ -1,9 +0,0 @@
-package com.fileuploader.upload.repositories;
-
-import com.fileuploader.upload.dataclasses.UserData;
-
-public interface UserRepositoryI {
-    public UserData createUser(String username, String password);
-
-    public UserData getUserByEmail(String email);
-}
